a2174a1cf57db27de4b635b6af797563ca97505e
Achsan <achsanh@gmail.com>
2024-11-09 22:37:37 +0000

    🛂 feat(auth): implement middleware to redirect if not logged in

--- app/composables/session.ts
+++ app/composables/session.ts
@@ -14,20 +14,9 @@ export function useUserSession(): UserSessionComposable {
 }
 
 async function fetch() {
-  // // https://github.com/nuxt/nuxt/issues/24813
-  // useSessionState().value = await useRequestFetch()("/api/auth/session").catch(
-  //   () => ({})
-  // );
-
-  // https://github.com/nuxt/nuxt/issues/21772#issuecomment-1606272783
-  // https://github.com/nuxt/nuxt/issues/23434#issuecomment-1737484680
-  // https://github.com/nuxt/nuxt/issues/25099#issuecomment-1881116359
-  // https://github.com/nuxt/nuxt/issues/28485#issuecomment-2283319706
-  const nuxtApp = useNuxtApp();
-  const { data } = await useFetch("/api/auth/session");
-  await nuxtApp.runWithContext(() => {
-    useSessionState().value = data.value!;
-  });
+  useSessionState().value = await useRequestFetch()("/api/auth/session").catch(
+    () => ({})
+  );
 }
 
 async function clear() {
