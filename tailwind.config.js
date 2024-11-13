cff15ce1f75d5281a070f94483980d47617deabf
Achsan <achsanh@gmail.com>
2024-11-13 08:21:21 +0000

    👔 feat(document): add document BAPP page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
-const animate = require("tailwindcss-animate")
+const animate = require("tailwindcss-animate");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
   safelist: ["dark"],
   prefix: "tw-",
-  
   theme: {
     container: {
       center: true,
@@ -67,10 +66,10 @@ module.exports = {
         },
         "collapsible-down": {
           from: { height: 0 },
-          to: { height: 'var(--radix-collapsible-content-height)' },
+          to: { height: "var(--radix-collapsible-content-height)" },
         },
         "collapsible-up": {
-          from: { height: 'var(--radix-collapsible-content-height)' },
+          from: { height: "var(--radix-collapsible-content-height)" },
           to: { height: 0 },
         },
       },
@@ -80,7 +79,10 @@ module.exports = {
         "collapsible-down": "collapsible-down 0.2s ease-in-out",
         "collapsible-up": "collapsible-up 0.2s ease-in-out",
       },
+      fontFamily: {
+        montserrat: ["Montserrat"],
+      },
     },
   },
   plugins: [animate],
-}
+};
