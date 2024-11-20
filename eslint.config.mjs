279a58966404316f7138c7d0a54e15372eddaddc
Achsan <achsanh@gmail.com>
2024-11-20 06:53:57 +0000

    🔧 chore(linter, formatter): remove Prettier and fully implement ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,17 @@
 // @ts-check
+import tailwind from 'eslint-plugin-tailwindcss';
 import withNuxt from './.nuxt/eslint.config.mjs';
 
-export default withNuxt({
-  ignores: ['app/components/shadcn'],
-});
+export default withNuxt(
+  {
+    ignores: ['app/components/shadcn'],
+  },
+  ...tailwind.configs['flat/recommended'],
+  {
+    settings: {
+      tailwindcss: {
+        whitelist: ['aik\\-[a-zA-Z]+', 'g_id_signin'],
+      },
+    },
+  },
+);
