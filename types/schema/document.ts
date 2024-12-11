a5fd6f09beabf094b930b16de69f17afe362bc57
Achsan <achsanh@gmail.com>
2024-12-11 10:13:31 +0000

    🏷️ feat(document): flatten workDocument structure by removing nested objects

--- types/schema/document.ts
+++ types/schema/document.ts
@@ -1,55 +1,30 @@
 import { z } from 'zod';
 
-export const signSchema = z.object({
-  url: z.string(),
-});
-export type Sign = z.infer<typeof signSchema>;
-
 export const employeeInfoSchema = z.object({
-  name: z.string(),
-  role: z.string(),
-  sign: signSchema,
-  supervisor: z.object({
-    name: z.string(),
-    role: z.string(),
-    sign: signSchema,
-    phone: z.number().optional(),
-  }),
+  employeeName: z.string(),
+  employeeRole: z.string(),
+  employeeSignUrl: z.string(),
+  supervisorName: z.string(),
+  supervisorRole: z.string(),
+  supervisorSignUrl: z.string(),
 });
 export type EmployeeInfo = z.infer<typeof employeeInfoSchema>;
 
-export const workDocumentSchema = z.object({
-  details: z.object({
-    title: z.string(),
-    date: z.object({
-      ts: z.object({
-        start: z.number(),
-        end: z.number(),
-      }),
-      date: z.object({
-        start: z.string(),
-        end: z.string(),
-      }),
-    }),
-  }),
-  employee: employeeInfoSchema,
-  po: z.object({
-    number: z.string(),
-  }),
-  bapp: z.object({
-    number: z.string(),
-    date: z.string(),
-    date_ts: z.number(),
-  }),
-  invoice: z.object({
-    number: z.string(),
-    nominal: z.number().optional(),
-    date: z.string(),
-    date_ts: z.number(),
-  }),
-  bast: z
-    .object({
-      number: z.string().optional(),
-    }),
+export const workDocumentSchema = employeeInfoSchema.extend({
+  detailsTitle: z.string(),
+  detailsDateStart: z.string(),
+  detailsDateEnd: z.string(),
+  detailsDateTsStart: z.number(),
+  detailsDateTsEnd: z.number(),
+
+  poNumber: z.string(),
+  bappNumber: z.string(),
+  bappDate: z.string(),
+  bappDateTs: z.number(),
+  invoiceNumber: z.string(),
+  invoiceNominal: z.number().optional(),
+  invoiceDate: z.string(),
+  invoiceDateTs: z.number(),
+  bastNumber: z.string().optional(),
 });
 export type WorkDocument = z.infer<typeof workDocumentSchema>;
