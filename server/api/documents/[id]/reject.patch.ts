9cdd0ec6688a6a14fc06396aa321eb99bee89a33
Achsan <achsanh@gmail.com>
2024-12-17 01:59:12 +0000

    🐛 fix(document): update API handling for new database structure
    With the introduction of the `original` column, we are able to:
    - Remove the `type` validation.
    - Simplify the query by eliminating the `type` condition from the queries.

--- server/api/documents/[id]/reject.patch.ts
+++ server/api/documents/[id]/reject.patch.ts
@@ -1,19 +1,12 @@
 export default defineEventHandler(async (event) => {
   const id = decodeURI(getRouterParam(event, 'id') || '');
-  const type = decodeURI(getRouterParam(event, 'type') || '').toLowerCase();
 
-  if (!isValidDocumentType(type)) throw createError({ statusCode: 404 });
   await verifyUserAuthorizationByRole(event, { role: ['admin'] });
 
   const workDocument = await useDB()
     .update(tables.documentMitra)
     .set({ isValidated: true, isApproved: false, validatedAt: new Date(), revisedAt: null })
-    .where(
-      and(
-        eq(tables.documentMitra.type, type),
-        eq(tables.documentMitra.id, id),
-      ),
-    )
+    .where(eq(tables.documentMitra.id, id))
     .returning()
     .get();
 
