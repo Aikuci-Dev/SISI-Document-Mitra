ad01ebf2b78d3f97db88ac7b335124e786b4c9f6
Achsan <achsanh@gmail.com>
2024-12-23 11:01:34 +0000

    ✨ feat(document): consolidate approve and reject handler into a single endpoint

--- server/api/documents/[id]/[approveOrReject].patch.ts
+++ server/api/documents/[id]/[approveOrReject].patch.ts
@@ -1,11 +1,15 @@
 export default defineEventHandler(async (event) => {
   const id = decodeURI(getRouterParam(event, 'id') || '');
 
+  const approveOrReject = decodeURI(getRouterParam(event, 'approveOrReject') || '');
+  if (!['approve', 'reject'].includes(approveOrReject.toLowerCase())) throw createError({ statusCode: 404 });
+  const isApproved = approveOrReject === 'approve';
+
   await verifyUserAuthorizationByRole(event, { role: ['admin'] });
 
   const workDocument = await useDB()
     .update(tables.documentMitra)
-    .set({ isValidated: true, isApproved: false, validatedAt: new Date(), revisedAt: null })
+    .set({ isValidated: true, isApproved, validatedAt: new Date(), revisedAt: null })
     .where(eq(tables.documentMitra.id, id))
     .returning()
     .get();
