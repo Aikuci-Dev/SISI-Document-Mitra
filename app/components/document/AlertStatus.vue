3da36e698123d4d537afacd767fc0e4894cb61ce
Achsan <achsanh@gmail.com>
2024-12-31 09:44:17 +0000

    ✨ refactor(status): change variant classes to use Partial<Record> for better flexibility

--- app/components/document/AlertStatus.vue
+++ app/components/document/AlertStatus.vue
@@ -24,26 +24,15 @@ const alertTitle = computed(() => {
   return title;
 });
 
-const variantClassesByStatus: Record<STATUSES_TYPE, string> = {
-  nil: '',
-  draft: '',
-  initiated: '',
+const variantClassesByStatus: Partial<Record<STATUSES_TYPE, string>> = {
   created: 'border-slate-900/50 text-slate-900 dark:border-slate-900 [&>svg]:text-slate-900 dark:border-slate-50/50 dark:text-slate-50 dark:dark:border-slate-50 dark:[&>svg]:text-slate-50',
   approved: 'border-green-500/50 text-green-500 dark:border-green-500 [&>svg]:text-green-500 dark:border-green-900/50 dark:text-green-900 dark:dark:border-green-900 dark:[&>svg]:text-green-900',
   rejected: 'border-slate-900/50 text-slate-900 dark:border-slate-900 [&>svg]:text-slate-900 dark:border-slate-50/50 dark:text-slate-50 dark:dark:border-slate-50 dark:[&>svg]:text-slate-50',
-  revised: '',
   signed: 'border-blue-500/50 text-blue-500 dark:border-blue-500 [&>svg]:text-blue-500 dark:border-blue-900/50 dark:text-blue-900 dark:dark:border-blue-900 dark:[&>svg]:text-blue-900',
 };
 
-const variantAlertByStatus: Record<STATUSES_TYPE, AlertVariants['variant']> = {
-  nil: 'default',
-  draft: 'default',
-  initiated: 'default',
-  created: 'default',
-  approved: 'default',
+const variantAlertByStatus: Partial<Record<STATUSES_TYPE, AlertVariants['variant']>> = {
   rejected: 'destructive',
-  revised: 'default',
-  signed: 'default',
 };
 </script>
 
